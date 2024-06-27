8293f83d2b6236076a2c5f225a0bbbb78354c6f1
Zonia Ashfaq <163324922+zoniaashfaq@users.noreply.github.com>
2024-06-27 21:05:28 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+import inquirer from "inquirer";
+//----------ATM MACHINE--------------
+let Balance = 50000;
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "YourPin",
+        message: "Enter your pin number",
+        type: "number",
+    },
+]);
+if (pinAnswer.YourPin === myPin) {
+    let answer = await inquirer.prompt([
+        {
+            name: "accountType",
+            message: "Select your account type",
+            type: "list",
+            choices: ["Current Account", "Savings Account"],
+        },
+        {
+            name: "Transaction",
+            message: "Select your transaction method",
+            type: "list",
+            choices: ["Cash Withdrawl", "Fast Cash"]
+        },
+    ]);
+    if (answer.Transaction === "Cash Withdrawl") {
+        let answer = await inquirer.prompt([
+            {
+                name: "Withdrawl",
+                message: "Enter the amount you want to withdraw",
+                type: "input",
+            }
+        ]);
+        if (Balance >= answer.Withdrawl) {
+            Balance -= answer.Withdrawl;
+            console.log(`💲 Your remainin balance is ${Balance} 💲`);
+        }
+        else {
+            console.log(`❗❗ NOT SUFFIECIENT BALANCE ❗❗`);
+        }
+    }
+    else {
+        let fastCash = await inquirer.prompt([
+            {
+                name: "fastCash",
+                message: "Enter the ammount you want to withdraw",
+                type: "list",
+                choices: ["1000", "5000", "10000", "20000"]
+            },
+        ]);
+        if (Balance >= fastCash.fastCash) {
+            Balance -= fastCash.fastCash;
+            console.log(`💲 Your remainin balance is ${Balance} 💲`);
+        }
+        else {
+            console.log(`❗❗ NOT SUFFIECIENT BALANCE ❗❗`);
+        }
+    }
+}
